c15f7671a4007152a38f6244b03d28f51e61f76b
LuciaYangYang <a20170056@juan23.edu.pe>
2024-10-23 18:11:21 -0500

    Add files via upload

---
+++ app01.js
@@ -0,0 +1,3 @@
+document.write("<h1>Bienvenida a la pagina de JavaScrip</h1>");
+var nom = prompt("Ingrese su nombre");
+document.write("Hola " + nom + " es tu primer contacto con la programacion");
